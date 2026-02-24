b1628dc50411d8c2741b04f7c83363e377bac80d
Alejandro Santillan <contact@alejandrops.dev>
2026-02-24 08:46:38 -0600

    Adding Skills partial view section

---
+++ dev-showcase/Views/Home/Sections/_Skills.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace dev_showcase.Views.Home.Sections
+{
+    public class _SkillsModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
