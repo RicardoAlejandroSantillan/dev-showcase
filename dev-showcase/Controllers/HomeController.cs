f9ab0af1915a3b772030534ae758da54d616c381
Alejandro Santillan <contact@alejandrops.dev>
2026-02-24 07:01:00 -0600

    Starting proyect

---
+++ dev-showcase/Controllers/HomeController.cs
@@ -0,0 +1,25 @@
+using dev_showcase.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace dev_showcase.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
